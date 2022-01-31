10ddaca996680f1a5e02c820fe9a13d630f65046
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-01-31 23:06:55 +0300

    Changed field name in State class

--- project/include/State.hpp
+++ project/include/State.hpp
@@ -8,6 +8,6 @@ namespace crypto {
 struct State {
   State() = default;
   [[maybe_unused]] explicit State(const std::string_view &block);
-  std::array<std::array<uint8_t, NUMBER_OF_COLUMNS>, NUMBER_OF_STATE_ROWS> stateTable;
+  std::array<std::array<uint8_t, NUMBER_OF_COLUMNS>, NUMBER_OF_STATE_ROWS> blockTable;
 };
 } // namespace crypto
--- project/src/State.cpp
+++ project/src/State.cpp
@@ -1,5 +1,5 @@
 #include "State.hpp"
 
 namespace crypto {
-State::State([[maybe_unused]] const std::string_view &block) : stateTable() {}
+State::State([[maybe_unused]] const std::string_view &block) : blockTable() {}
 } // namespace crypto
