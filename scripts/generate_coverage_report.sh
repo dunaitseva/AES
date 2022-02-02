9fca5f875b82cf86771076d46aa71744bea8cac9
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-02-03 00:14:31 +0300

    Including codecov options in workflow

--- scripts/generate_coverage_report.sh
+++ scripts/generate_coverage_report.sh
@@ -5,18 +5,20 @@ pathBaseCryptoUtilsTests="./build/CMakeFiles/BaseCryptoUtilsTests.dir/tests/Base
 pathRijndaelTests="./build/CMakeFiles/RijndaelTests.dir/tests/RijndaelTests.cpp"
 pathKeyScheduleTests="./build/CMakeFiles/KeyScheduleTests.dir/tests/KeyScheduleTests.cpp"
 
+rm -rf cov
 mkdir cov
 
 cp $pathStateTests.gcda ./cov/StateTests.cpp.gcda
-cp $pathStateTests.gcda ./cov/BaseCryptoUtilsTests.cpp.gcda
-cp $pathStateTests.gcda ./cov/RijndaelTests.cpp.gcda
-cp $pathStateTests.gcda ./cov/KeyScheduleTests.cpp.gcda
+cp $pathBaseCryptoUtilsTests.gcda ./cov/BaseCryptoUtilsTests.cpp.gcda
+cp $pathRijndaelTests.gcda ./cov/RijndaelTests.cpp.gcda
+cp $pathKeyScheduleTests.gcda ./cov/KeyScheduleTests.cpp.gcda
 
 cp $pathStateTests.gcno ./cov/StateTests.cpp.gcno
-cp $pathStateTests.gcno ./cov/BaseCryptoUtilsTests.cpp.gcno
-cp $pathStateTests.gcno ./cov/RijndaelTests.cpp.gcno
-cp $pathStateTests.gcno ./cov/KeyScheduleTests.cpp.gcno
+cp $pathBaseCryptoUtilsTests.gcno ./cov/BaseCryptoUtilsTests.cpp.gcno
+cp $pathRijndaelTests.gcno ./cov/RijndaelTests.cpp.gcno
+cp $pathKeyScheduleTests.gcno ./cov/KeyScheduleTests.cpp.gcno
 
 cd cov
-lcov -t "Test Coverage" --capture --external -o test-coverage.info -c -d .
-#genhtml test-coverage.info --branch-coverage --output-directory ./code_coverage_report/
+lcov -t "Test Coverage" -o test-coverage.info -c -d .
+lcov --list test-coverage.info
+genhtml test-coverage.info --branch-coverage --output-directory ./code_coverage_report/
