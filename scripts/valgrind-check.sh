750db9d2fda560d9ed6a7fb1e73e0630e01d12c1
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-02-05 20:07:18 +0300

    Fixed valgrind-check.sh

--- scripts/valgrind-check.sh
+++ scripts/valgrind-check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt install valgrind > /dev/null && echo "valgrind is already installed" || echo "Error while installing valgrind"
+
 BUILD_DIR="./build"
 VALGRIND_LOG_FILE="./tmp/valgrind-check.log"
 TMP_DIR="./tmp"
@@ -13,7 +15,7 @@ diagnostic_message () {
   echo "Error, $1 valgrind check. Check log file $VALGRIND_LOG_FILE."
 }
 
-VALGRIND_OPTIONS='--tool=memcheck --leak-check=full --track-origins=yes -s'
+VALGRIND_OPTIONS="--tool=memcheck --leak-check=full --track-origins=yes -s --log-file=$VALGRIND_LOG_FILE"
 
 # Тут нужно будет дописать диагностические сообщения
 #valgrind --tool=memcheck ./$BUILD_DIR/StateTests > $VALGRIND_LOG_FILE || (diagnostic_message "StateTests" && exit 1)
@@ -21,13 +23,13 @@ VALGRIND_OPTIONS='--tool=memcheck --leak-check=full --track-origins=yes -s'
 #valgrind --tool=memcheck ./$BUILD_DIR/KeyScheduleTests > $VALGRIND_LOG_FILE || (diagnostic_message "KeyScheduleTests" && exit 1)
 #valgrind --tool=memcheck ./$BUILD_DIR/RijndaelTests > $VALGRIND_LOG_FILE || (diagnostic_message "RijndaelTests" && exit 1)
 echo "-----> START StateTests CHECK"
-valgrind $VALGRIND_OPTIONS ./$BUILD_DIR/StateTests > $VALGRIND_LOG_FILE || (diagnostic_message "StateTests" && exit 1)
+valgrind $VALGRIND_OPTIONS ./$BUILD_DIR/StateTests >> /dev/null || (diagnostic_message "StateTests" && exit 1)
 
 echo "-----> START BaseCryptoUtilsTests CHECK"
-valgrind $VALGRIND_OPTIONS ./$BUILD_DIR/BaseCryptoUtilsTests > $VALGRIND_LOG_FILE || (diagnostic_message "BaseCryptoUtilsTests" && exit 1)
+valgrind $VALGRIND_OPTIONS ./$BUILD_DIR/BaseCryptoUtilsTests >> /dev/null || (diagnostic_message "BaseCryptoUtilsTests" && exit 1)
 
 echo "-----> START KeyScheduleTests CHECK"
-valgrind $VALGRIND_OPTIONS ./$BUILD_DIR/KeyScheduleTests > $VALGRIND_LOG_FILE || (diagnostic_message "KeyScheduleTests" && exit 1)
+valgrind $VALGRIND_OPTIONS ./$BUILD_DIR/KeyScheduleTests >> /dev/null || (diagnostic_message "KeyScheduleTests" && exit 1)
 
 echo "-----> START RijndaelTests CHECK"
-valgrind $VALGRIND_OPTIONS ./$BUILD_DIR/RijndaelTests > $VALGRIND_LOG_FILE || (diagnostic_message "RijndaelTests" && exit 1)
+valgrind $VALGRIND_OPTIONS ./$BUILD_DIR/RijndaelTests >> /dev/null || (diagnostic_message "RijndaelTests" && exit 1)
