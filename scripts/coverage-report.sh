e7bf48793d9176b3eeb29db46bdad8c227255b5c
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-02-06 02:11:49 +0300

    trying to execute ci

--- scripts/coverage-report.sh
+++ scripts/coverage-report.sh
@@ -9,8 +9,8 @@ pip install gcovr
 cd $BUILD_DIRECTORY || (echo "Error. $BUILD_DIRECTORY not created." && exit 1)
 ctest
 cd ..
-#EXCLUDING_FILES=".*_deps.*|.*tests.*"
-EXCLUDING_FILES=".*_deps.*"
+EXCLUDING_FILES=".*_deps.*|.*tests.*"
+#EXCLUDING_FILES=".*_deps.*"
 REPORT_FILE_NAME="coverage.xml"
 gcovr -r . ./build -e $EXCLUDING_FILES -o $REPORT_FILE_NAME --xml
 #gcovr -r . ./build -e $EXCLUDING_FILES -o cov.html --html
